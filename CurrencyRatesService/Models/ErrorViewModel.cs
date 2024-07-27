ff45580236a041fb1e070c712d2bbdcdf05fb9d0
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2024-07-27 15:13:13 +0300

    create new currency rates updater

---
+++ CurrencyRatesService/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CurrencyRatesService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ CurrencyRatesService/entrypoint.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+set -e
+
+echo "Starting entrypoint script"
+
+# Navigate back to the app directory
+cd /app
+
+# Run the application
+echo "Starting the application..."
+exec dotnet CurrencyRatesService.dll
