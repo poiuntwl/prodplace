95ff146b365b53d49fba2cc1ce0c8b0985fa1392
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2024-09-16 10:06:49 +0300

    feature/docker-compose-organization fix bat script

--- compose-up.bat
+++ compose-up.bat
@@ -1,13 +1,10 @@
 @echo off
    REM Save this as docker-compose-reset.bat
 
-   docker-compose down --volumes
+   docker-compose down
    if %ERRORLEVEL% neq 0 goto :error
 
-   docker-compose build --no-cache
-   if %ERRORLEVEL% neq 0 goto :error
-
-   docker-compose up
+   docker-compose -f docker-compose.yml -f .\docker-compose.db.yml -f .\docker-compose.app.yml up --build
    if %ERRORLEVEL% neq 0 goto :error
 
    goto :EOF
