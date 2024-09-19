53c889394a8a6774464f6bf231ba5f2e4a087947
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2024-09-19 23:49:22 +0300

    develop fix dockerfiles and inits a bit

--- dbs/currencies/healthcheck.sh
+++ dbs/currencies/healthcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "IF DB_ID('ProductsDB') IS NOT NULL and DB_ID('HangfireDB') IS NOT NULL SELECT 1 ELSE SELECT 0"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "IF DB_ID('CurrencyRatesDB') IS NOT NULL and DB_ID('HangfireDB') IS NOT NULL SELECT 1 ELSE SELECT 0"
