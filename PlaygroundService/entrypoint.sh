16950acd1c92a5b71f110d268f5f9693160b414b
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2024-10-30 23:45:31 +0200

    develop remove playground service

--- PlaygroundService/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-PROJECT_NAME="$1"
-echo "Starting entrypoint script for $PROJECT_NAME"
-
-# Run the application
-echo "Starting the application: $PROJECT_NAME"
-exec dotnet "$PROJECT_NAME.dll"
