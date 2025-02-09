8370f86e7c79f1946f356bd3231763f749a0a54e
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2025-02-09 22:49:13 +0200

    feature/improve-auth-v2 visual improvements

--- ProxyService/Program.cs
+++ ProxyService/Program.cs
@@ -4,53 +4,41 @@ using AuthTools.Services;
 using ProxyService;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-var s = builder.Services;
-s.AddJwtAuthConfiguration(builder);
-s.AddJwtAuthServices();
-s.AddSingleton<IGatewayService, GatewayService>();
-
-s.AddEndpointsApiExplorer();
-s.AddSwaggerGen();
-s.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
-s.AddCors(x =>
-{
-    x.AddPolicy("VueCorsPolicy",
-        y => { y.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:12345"); });
-});
+services.AddJwtAuthConfiguration(builder);
+services.AddJwtAuthServices();
+services.AddSingleton<IGatewayService, GatewayService>();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+services.AddCors(x => x.AddPolicy("VueCorsPolicy", y =>
+    y.AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins("http://localhost:12345")));
 
 var app = builder.Build();
+
 app.UseJwtAuthConfiguration();
 app.UseCors("VueCorsPolicy");
 
-app.MapPost("/login",
-    async (LoginRequest creds, IKeycloakHttpClient keycloakHttpClient, CancellationToken ct) =>
-    {
-        var accessToken = await keycloakHttpClient.GetAccessTokenAsync(creds.Email, creds.Password, ct);
+app.MapPost("/login", async (LoginRequest creds, IKeycloakHttpClient client, CancellationToken ct) =>
+    Results.Ok(await client.GetAccessTokenAsync(creds.Email, creds.Password, ct)));
 
-        return Results.Ok(accessToken);
-    });
-
-app.MapPost("/refresh",
-    async (RefreshRequest request, IKeycloakHttpClient keycloakHttpClient, CancellationToken ct) =>
-    {
-        var accessToken = await keycloakHttpClient.RefreshTokenAsync(request.RefreshToken, ct);
-
-        return Results.Ok(accessToken);
-    });
+app.MapPost("/refresh", async (RefreshRequest request, IKeycloakHttpClient client, CancellationToken ct) =>
+    Results.Ok(await client.RefreshTokenAsync(request.RefreshToken, ct)));
 
 app.MapGet("/hello", () => "Hello World!").RequireAuthorization();
 app.MapGet("/hello-insecure", () => "Hello World!");
 
 app.MapReverseProxy(x =>
 {
-    var gatewayService = x.ApplicationServices.GetRequiredService<IGatewayService>();
+    var gateway = x.ApplicationServices.GetRequiredService<IGatewayService>();
     x.Use(async (ctx, next) =>
     {
-        await gatewayService.AddTokenAsync(ctx, ctx.RequestAborted);
-        await next().ConfigureAwait(false);
+        await gateway.AddTokenAsync(ctx, ctx.RequestAborted);
+        await next();
     });
 });
 
@@ -60,24 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
 app.UseHttpsRedirection();
-
 app.Run();
 
 internal record LoginRequest(string Email, string Password);
-
-internal record RefreshRequest
-{
-    public RefreshRequest(string RefreshToken)
-    {
-        this.RefreshToken = RefreshToken;
-    }
-
-    [JsonPropertyName("refresh_token")] public string RefreshToken { get; init; }
-
-    public void Deconstruct(out string RefreshToken)
-    {
-        RefreshToken = this.RefreshToken;
-    }
-}
+internal record RefreshRequest([property: JsonPropertyName("refresh_token")] string RefreshToken);
