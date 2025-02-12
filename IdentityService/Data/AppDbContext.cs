caf0df71551412d4707ce2371a34a9fd4e562437
Volodymyr <cont_vlenio@cont.plasticsurgery.org>
2025-02-13 01:10:23 +0200

    feature/improve-auth-v2 remove identity-related packages

--- IdentityService/Data/AppDbContext.cs
+++ IdentityService/Data/AppDbContext.cs
@@ -2,12 +2,11 @@
 using IdentityService.Models;
 using MassTransit;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityService.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
